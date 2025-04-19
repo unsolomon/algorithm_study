07bf4d207604c96cef996ba8989395ae8610f291
JeonginLee <unsolomon1001@gmail.com>
2025-04-20 03:22:39 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    answer = []
+    a = 0
+    b = 0
+    for i in num_list:
+        if i %2 == 0:
+            a += 1
+        else :
+            b+=1
+    
+    
+    return [a,b]
