7d19ed268d2789fd28968a3d4477da9dad6baebd
JeonginLee <unsolomon1001@gmail.com>
2025-05-02 17:21:19 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,9 @@
+def solution(price):
+    if price >= 500000:
+        return int(price - (price * 0.2))
+    elif price >= 300000:
+        return int(price - (price * 0.1))
+    elif price >= 100000:
+        return int(price - (price * 0.05))
+    else:
+        return price
