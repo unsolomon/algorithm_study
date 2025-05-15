1c5e010f9d9e71e9c0f7ee2f5987a19819a9a59a
JeonginLee <unsolomon1001@gmail.com>
2025-05-15 16:50:36 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,10 +1,3 @@
 def solution(num, k):
-    answer = str(num)
-    
-    a = 0
-    
-    for i in answer:
-        a += 1
-        if k == int(i):
-            return a
-    return -1
+    answer = 0
+    return answer
