e0ee6004621451a11574d9620ecd0598b767c873
JeonginLee <unsolomon1001@gmail.com>
2025-05-27 13:26:37 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -0,0 +1,9 @@
+def solution(todo_list, finished):
+    answer = []
+    
+    for todo, is_finished in zip(todo_list,finished):
+        if not is_finished:
+            answer.append(todo)
+    
+    
+    return answer
