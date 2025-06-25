1dc14e9c13fe3c44a3959b9235f3ad0aabb02b71
JeonginLee <unsolomon1001@gmail.com>
2025-06-25 18:42:26 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,10 +1,2 @@
 def solution(myString):
-    answer = ''
-    for i in myString:
-        if i == 'a':
-            answer += 'A'
-        elif i.isupper() and i != 'A':
-            answer += i.lower()
-        else:
-            answer += i
-    return answer
+    return myString.lower().replace('a','A')
