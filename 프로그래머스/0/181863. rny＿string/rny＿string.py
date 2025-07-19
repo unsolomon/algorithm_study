b9122d7559bddf955350d42b8cc13f75910b1487
JeonginLee <unsolomon1001@gmail.com>
2025-07-19 11:00:46 +0900

    [level 0] Title: rny_string, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -0,0 +1,11 @@
+def solution(rny_string):
+    answer = []
+    
+    for i in rny_string:
+        if i == 'm':
+            answer.append('rn')
+        else:
+            answer.append(i)
+    
+    
+    return ''.join(answer)
