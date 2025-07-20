11535eb83b559712af313be0f5541b66693fee98
JeonginLee <unsolomon1001@gmail.com>
2025-07-20 17:47:19 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,9 +1,7 @@
-
-
 def solution(n):
     answer = 0
-    while n > 0:        
-        answer += n%10
-        n //= 10
-
+    
+    for i in str(n):
+        answer += int(i)
+    
     return answer
