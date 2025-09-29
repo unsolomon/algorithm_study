7de79a01eece42daabb111e59e0f508ea368b312
JeonginLee <unsolomon1001@gmail.com>
2025-09-29 21:02:11 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.80 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,6 +1,7 @@
 def solution(n):
-    x = n ** 0.5                 # n의 제곱근
-    if x == int(x):              # 제곱근이 정수라면 (즉, x가 양의 정수)
-        return int(x+1) ** 2     # (x+1)의 제곱
+    answer = n**0.5
+    
+    if answer == int(answer) :
+        return (answer+1)**2
     else:
         return -1
