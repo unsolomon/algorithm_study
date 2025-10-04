4ffc28a88b575adc40a5388f8f21e77b70224087
JeonginLee <unsolomon1001@gmail.com>
2025-10-04 21:04:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,15 +1,15 @@
 def solution(num):
     answer = 0
     
-    while num != 1:
+    while num != 1 :
         if answer >= 500:
             return -1
-        if num %2 == 0:
-            num //= 2
+        elif num % 2 == 0:
+            num = num // 2
         else:
-            num = num*3 +1 
+            num = num*3 +1
+        
         answer += 1
     
-    
-    
     return answer
+
