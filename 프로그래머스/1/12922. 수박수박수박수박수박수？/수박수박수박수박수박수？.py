a30e509d94b0c90413fd89a022ed2839e50c5f3b
JeonginLee <unsolomon1001@gmail.com>
2025-10-09 16:29:47 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.17 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -3,10 +3,10 @@ def solution(n):
     
     for i in range(n):
         
-        if i %2 == 0:
+        if i % 2== 0:
             answer += '수'
         else:
-            answer +='박'
+            answer += '박'
     
     
     return answer
