3bf7ec07c0ca80ce7cb0386f29838ded98349fa7
JeonginLee <unsolomon1001@gmail.com>
2025-10-19 15:25:12 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -2,22 +2,17 @@ def solution(d, budget):
     answer = 0
     count = 0 
     d.sort()
-    
+
     for i in d:
         answer += i
-        if answer <= budget:
-            count += 1
-        else:
+        if budget < answer:
             break
             
+            
+        count += 1
+    
+    
     return count
 
 
 
-
-# s사에서 각 부서당 구매물풀 조사
-#예산한저ㅏㅇ
-#각 부서 최소 금액지원
-# 각 주서별 지원 금 d
-#예산 budge
-# 최대 몇개의 부서에 물품 지원
