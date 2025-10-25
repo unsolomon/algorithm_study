89ee476634526c12fa2b0e79159663f457a2258d
JeonginLee <unsolomon1001@gmail.com>
2025-10-25 16:21:29 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.42 ms, Memory: 9.81 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,24 +1,14 @@
 def solution(s):
     answer = []
-    last_positions = {}  
-    
+    idex = {}
     for i,ch in enumerate(s):
-        if ch not in last_positions:
-            answer.append(-1)
+        if ch in idex:
+            answer.append(i-idex[ch])
         else:
-            answer.append(i -last_positions[ch] )
+            answer.append(-1)
+            
+        idex[ch] = i
         
-        last_positions[ch] = i  # 현재 위치로 업데이트
-    
-    
-    
+        
     return answer
-
-
-
-
-# 1.문자열 s의 각 s의 각 위치보다 앞에 나왔으며 가장 가까운 곳에 있는 같은 글자
-
-
-
-
+            
