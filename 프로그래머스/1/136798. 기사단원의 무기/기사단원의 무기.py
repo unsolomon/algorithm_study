419c514451cdf2024e6c624ab8b26cfa5ba47813
JeonginLee <unsolomon1001@gmail.com>
2025-11-07 18:52:39 +0900

    [level 1] Title: 기사단원의 무기, Time: 1110.16 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,18 +1,17 @@
 def solution(number, limit, power):
-    def count_divisors(n):
+    def count_d(n):
         cnt = 0
         for i in range(1, int(n**0.5)+1):
-            if n % i == 0:
-                cnt += 1
-                if i != n // i:
-                    cnt += 1
+            if n%i ==0:
+                cnt +=2 if i != n// i else 1
         return cnt
-
-    total = 0
-    for knight in range(1, number+1):
-        attack = count_divisors(knight)
-        if attack > limit:
-            attack = power
-        total += attack
-
-    return total
+    
+    total_weight = 0 
+    for i in range(1, number+1):
+        div_count = count_d(i)
+        if div_count > limit:
+            total_weight += power
+        else:
+            total_weight += div_count 
+    
+    return total_weight
