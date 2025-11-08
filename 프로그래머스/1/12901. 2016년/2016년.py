d563d6ff76990b1d0958b4a3f46a1d4e001b9cbd
JeonginLee <unsolomon1001@gmail.com>
2025-11-08 23:07:59 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,8 +1,11 @@
 import datetime
 
 def solution(a, b):
-    days = ["MON","TUE","WED","THU","FRI","SAT","SUN"]
-
-    date = datetime.date(2016, a, b)
-
-    return days[date.weekday()]
+    days = ['SUN',"MON","TUE","WED","THU","FRI","SAT"]
+    date = datetime.date(2016,a,b)
+    
+    
+    
+    
+    
+    return days[(date.weekday() +1)%7]
