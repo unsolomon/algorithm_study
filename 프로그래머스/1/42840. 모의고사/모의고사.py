ee3dd617c6ae9f94cec7f5962b6185b478de8dc8
JeonginLee <unsolomon1001@gmail.com>
2025-11-09 19:11:26 +0900

    [level 1] Title: 모의고사, Time: 3.06 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,25 +1,21 @@
 def solution(answers):
-    so1 = [1,2,3,4,5]
-    so2 = [2,1,2,3,2,4,2,5]
-    so3 = [3,3,1,1,2,2,4,4,5,5]
 
-    count1, count2, count3 = 0, 0, 0
+    supo1 = [1, 2, 3, 4, 5]
+    supo2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    supo3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
-    for idx, ans in enumerate(answers):
-        if ans == so1[idx % len(so1)]:
-            count1 += 1
-        if ans == so2[idx % len(so2)]:
-            count2 += 1
-        if ans == so3[idx % len(so3)]:
-            count3 += 1
 
-    max_score = max(count1, count2, count3)
-    result = []
-    if count1 == max_score:
-        result.append(1)
-    if count2 == max_score:
-        result.append(2)
-    if count3 == max_score:
-        result.append(3)
+    score = [0, 0, 0]
+    for i, ans in enumerate(answers):
+        if ans == supo1[i % len(supo1)]:
+            score[0] += 1
+        if ans == supo2[i % len(supo2)]:
+            score[1] += 1
+        if ans == supo3[i % len(supo3)]:
+            score[2] += 1
 
-    return result
+
+    max_score = max(score)
+
+    result = [i + 1 for i, s in enumerate(score) if s == max_score]
+    return sorted(result)
