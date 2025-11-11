5bfea306a086585e4cc82186579581f1d37b483f
JeonginLee <unsolomon1001@gmail.com>
2025-11-11 22:13:57 +0900

    [level 1] Title: 과일 장수, Time: 88.66 ms, Memory: 20.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,11 +1,10 @@
 def solution(k, m, score):
-    # 점수 내림차순 정렬
     score.sort(reverse=True)
-    profit = 0
+    n = len(score)//m
+    answer = 0
     
-    # m개씩 묶어서 상자 만들기
-    for i in range(0, len(score) - m + 1, m):
-        box = score[i:i+m]
-        profit += min(box) * m   # 상자의 최저 점수 × m
+    for i in range(n):
+        answer += score[(i+1) * m-1] * m
+        
     
-    return profit
+    return answer
