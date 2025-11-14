0a15077ec2c21424ea56f924d9a595edeb1de967
JeonginLee <unsolomon1001@gmail.com>
2025-11-14 22:37:23 +0900

    [level 1] Title: 옹알이 (2), Time: 0.09 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,13 +1,14 @@
 def solution(babbling):
-    answer = ["aya", "ye", "woo", "ma"]
+    can = ["aya", "ye", "woo", "ma"]
     count = 0
-    
     for word in babbling:
-        if any(p*2 in word for p in answer):
-            continue
         temp = word
-        for p in answer:
-            temp = temp.replace(p, ' ')
-        if temp.strip() == '':
-            count += 1
+        for c in can:
+            if c*2 in temp:
+                break  
+        else:
+            for c in can:
+                temp = temp.replace(c, ' ')
+            if temp.strip() == '':
+                count += 1
     return count
