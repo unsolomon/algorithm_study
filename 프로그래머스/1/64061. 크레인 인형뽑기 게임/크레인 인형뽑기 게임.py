6ebf96b61780cc918522bed611c62fbe1b9decc9
JeonginLee <unsolomon1001@gmail.com>
2025-11-27 23:15:40 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.03 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -0,0 +1,18 @@
+def solution(board, moves):
+    answer = 0
+    basket = []
+    n = len(board)
+
+    for move in moves:
+        col = move - 1 
+        for row in range(n):
+            if board[row][col] != 0:
+                doll = board[row][col]
+                board[row][col] = 0  
+                if basket and basket[-1] == doll:
+                    basket.pop()
+                    answer += 2  
+                else:
+                    basket.append(doll)
+                break 
+    return answer
